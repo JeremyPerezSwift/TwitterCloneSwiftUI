75dda13057c4d26c0ebed440c441d834244fe943
JeremyPerezSwift <jeremyperez34000@gmail.com>
2021-11-16 08:26:33 +0100

    Creating a Tab View

--- TwitterCloneSwiftUI/ContentView.swift
+++ TwitterCloneSwiftUI/ContentView.swift
@@ -9,12 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            VStack {
-                Text("Hello, world!")
-                    .padding()
+        
+        NavigationView {
+            TabView {
+                Text("Feed")
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+                
+                Text("Search")
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("Search")
+                    }
+                
+                Text("Messages")
+                    .tabItem {
+                        Image(systemName: "envelope")
+                        Text("Messages")
+                    }
             }
+            
+//            Text("Home")
+//                .navigationBarTitleDisplayMode(.inline)
+//                .toolbar {
+//                    ToolbarItem(placement: .principal) {
+//                        HStack {
+//                            Text("Home").font(.headline)
+//                        }
+//                    }
+//                }
+            
+            .navigationBarTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+        
         }
+        
         
     }
 }
