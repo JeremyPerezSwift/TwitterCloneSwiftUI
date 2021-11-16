923acd15adab05bd3e9bcf927967094ae698b82c
JeremyPerezSwift <jeremyperez34000@gmail.com>
2021-11-16 07:35:10 +0100

    Initial Commit

---
+++ TwitterCloneSwiftUI/TwitterCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneSwiftUIApp.swift
+//  TwitterCloneSwiftUI
+//
+//  Created by Jérémy Perez on 16/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
