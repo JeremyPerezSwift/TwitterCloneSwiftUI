8ca56883b02dc7aa97f4d4f0bc0e22f70a863759
JeremyPerezSwift <jeremyperez34000@gmail.com>
2021-12-09 07:52:09 +0100

    Building registration screen

--- TwitterCloneSwiftUI/View/Authentification/LoginView.swift
+++ TwitterCloneSwiftUI/View/Authentification/LoginView.swift
@@ -12,70 +12,84 @@ struct LoginView: View {
     @State var password = ""
     
     var body: some View {
-        ZStack {
-            VStack {
-                Image("twitter")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 100, height: 100)
-                    .padding(.top, 100)
-                    .padding(.bottom, 20)
-                
-                VStack(spacing: 20) {
-                    CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelope")
-                        .padding()
-                        .background(Color(.init(white: 1, alpha: 0.15)))
-                        .cornerRadius(10)
-                        .foregroundColor(.white)
+        NavigationView {
+            ZStack {
+                VStack {
+                    Image("twitter")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .padding(.top, 100)
+                        .padding(.bottom, 20)
                     
-                    CustomSecureField(text: $password, placeholder: Text("Password"), imageName: "lock")
-                        .padding()
-                        .background(Color(.init(white: 1, alpha: 0.15)))
-                        .cornerRadius(10)
-                        .foregroundColor(.white)
-                }
-                .padding(.horizontal)
-                
-                HStack {
-                    Spacer()
-                    
-                    Button(action: {}) {
-                        Text("Forgot Password ?")
-                            .font(.footnote)
-                            .bold()
+                    VStack(spacing: 20) {
+                        CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelope")
+                            .padding()
+                            .background(Color(.init(white: 1, alpha: 0.15)))
+                            .cornerRadius(10)
+                            .foregroundColor(.white)
+                        
+                        CustomSecureField(text: $password, placeholder: Text("Password"), imageName: "lock")
+                            .padding()
+                            .background(Color(.init(white: 1, alpha: 0.15)))
+                            .cornerRadius(10)
                             .foregroundColor(.white)
                     }
-                }
-                .padding()
-                
-                Button(action: {}) {
-                    Text("Sign In")
-                        .font(.headline)
-                        .foregroundColor(Color("color-twitter"))
-                        .frame(width: 350, height: 50)
-                        .background(Color.white)
-                        .clipShape(Capsule())
-                }
-                
-                
-                Spacer()
-                
-                HStack {
-                    Button(action: {}) {
-                        Text("Don't have an account ?")
-                            .font(.system(size: 14))
-                        
-                        Text("Sign Up")
-                            .font(.system(size: 14, weight: .semibold))
-                    }
-                }
-                .foregroundColor(.white)
-                .padding(.bottom, 40)
+                    .padding(.horizontal)
                     
+                    HStack {
+                        Spacer()
+                        
+                        Button(action: {}) {
+                            Text("Forgot Password ?")
+                                .font(.footnote)
+                                .bold()
+                                .foregroundColor(.white)
+                        }
+                    }
+                    .padding()
+                    
+                    Button(action: {}) {
+                        Text("Sign In")
+                            .font(.headline)
+                            .foregroundColor(Color("color-twitter"))
+                            .frame(width: 350, height: 50)
+                            .background(Color.white)
+                            .clipShape(Capsule())
+                    }
+                    
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination: RegistrationView().navigationBarBackButtonHidden(true)) {
+                        HStack {
+                            Text("Don't have an account ?")
+                                .font(.system(size: 14))
+                            
+                            Text("Sign Up")
+                                .font(.system(size: 14, weight: .semibold))
+                        }
+                        .foregroundColor(.white)
+                        .padding(.bottom, 40)
+                    }
+                    
+//                    HStack {
+//                        Button(action: {}) {
+//                            Text("Don't have an account ?")
+//                                .font(.system(size: 14))
+//
+//                            Text("Sign Up")
+//                                .font(.system(size: 14, weight: .semibold))
+//                        }
+//                    }
+//                    .foregroundColor(.white)
+//                    .padding(.bottom, 40)
+                        
+                }
             }
-        }
-        .background(Color("color-twitter"))
+            .background(Color("color-twitter"))
         .ignoresSafeArea()
+        }
     }
 }
 
--- TwitterCloneSwiftUI/View/Authentification/RegistrationView.swift
+++ TwitterCloneSwiftUI/View/Authentification/RegistrationView.swift
@@ -8,8 +8,91 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    @State var fullname = ""
+    @State var email = ""
+    @State var username = ""
+    @State var password = ""
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                Image("plus")
+                    .resizable()
+                    .renderingMode(.template)
+                    .scaledToFill()
+                    .frame(width: 100, height: 100)
+                    .padding(.top, 100)
+                    .padding(.bottom, 20)
+                    .foregroundColor(.white)
+                
+                VStack(spacing: 20) {
+                    CustomTextField(text: $fullname, placeholder: Text("Full Name"), imageName: "person")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                    
+                    CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelope")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                    
+                    CustomTextField(text: $username, placeholder: Text("Username"), imageName: "person")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                    
+                    CustomSecureField(text: $password, placeholder: Text("Password"), imageName: "lock")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                }
+                .padding(.horizontal)
+                
+                Button(action: {}) {
+                    Text("Sign Up")
+                        .font(.headline)
+                        .foregroundColor(Color("color-twitter"))
+                        .frame(width: 350, height: 50)
+                        .background(Color.white)
+                        .clipShape(Capsule())
+                }
+                .padding()
+                
+                Spacer()
+                
+                Button(action: { mode.wrappedValue.dismiss() }) {
+                    HStack {
+                        Text("Already have an account ?")
+                            .font(.system(size: 14))
+                        
+                        Text("Sign In")
+                            .font(.system(size: 14, weight: .semibold))
+                    }
+                    .foregroundColor(.white)
+                    .padding(.bottom, 40)
+                }
+//                HStack {
+//                    Text("Already have an account ?")
+//                        .font(.system(size: 14))
+//
+//                    Text("Sign In")
+//                        .font(.system(size: 14, weight: .semibold))
+//                }
+//                .foregroundColor(.white)
+//                .padding(.bottom, 40)
+//                .onTapGesture {
+//                    mode.wrappedValue.dismiss()
+//                }
+                    
+            }
+        }
+        .background(Color("color-twitter"))
+        .ignoresSafeArea()
     }
 }
 
