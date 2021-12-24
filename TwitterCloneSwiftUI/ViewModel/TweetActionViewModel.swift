f44a605d4281188af0be233cc4a6c049261015bc
JeremyPerezSwift <jeremyperez34000@gmail.com>
2021-12-24 08:00:59 +0100

    Unlike Tweet | Check if user liked tweet

--- TwitterCloneSwiftUI/ViewModel/TweetActionViewModel.swift
+++ TwitterCloneSwiftUI/ViewModel/TweetActionViewModel.swift
@@ -13,6 +13,7 @@ class TweetActionViewModel: ObservableObject {
     
     init(tweet: Tweet) {
         self.tweet = tweet
+        checkIfUserLikedTweet()
     }
     
     func likeTweet() {
@@ -28,7 +29,24 @@ class TweetActionViewModel: ObservableObject {
     }
     
     func unlikeTweet() {
+        guard let uid = AuthViewModel.shared.userSession?.uid else { return }
         
+        COLLECTION_TWEETS.document(tweet.id).updateData(["likes": tweet.likes - 1]) { error in
+            COLLECTION_TWEETS.document(self.tweet.id).collection("tweet-likes").document(uid).delete { error2 in
+                COLLECTION_USERS.document(uid).collection("user-likes").document(self.tweet.id).delete { error3 in
+                    self.didLike = false
+                }
+            }
+        }
+    }
+    
+    func checkIfUserLikedTweet() {
+        guard let uid = AuthViewModel.shared.userSession?.uid else { return }
+        
+        COLLECTION_USERS.document(uid).collection("user-likes").document(self.tweet.id).getDocument { snapshot, error in
+            guard let didLike = snapshot?.exists else { return }
+            self.didLike = didLike
+        }
     }
     
 }
